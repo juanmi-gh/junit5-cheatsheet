0015fd3877a0a0e9cda54cfa52e0a0dd348e3cd9
juanmi <jmcabrera@copado.com>
2022-06-28 19:21:24 +0200

    Add junit5 & google.truth

---
+++ src/test/java/es/jm/MainTest.java
@@ -0,0 +1,16 @@
+package es.jm;
+
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void test() {
+
+    assertThat(true).isTrue();
+  }
+
+}
